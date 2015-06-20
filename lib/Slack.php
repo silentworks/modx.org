3e8c37ec0ba54757cc169d3a0cf886029628ed2b
Mark Hamstra <hello@markhamstra.com>
2015-06-20 22:36:51 +0200

    Coding style stuff

--- lib/Slack.php
+++ lib/Slack.php
@@ -10,24 +10,23 @@ class Slack
     protected $info;
     protected $cacheFile;
 
-    function __construct($team, $token)
+    public function __construct($team, $token)
     {
         $this->team = $team;
         $this->token = $token;
 
         $this->cacheFile = $_ENV['CACHE_DIR'] . 'slack_' . $team . 'info.json';
-        if (file_exists($this->cacheFile) && 
+        if (file_exists($this->cacheFile) &&
             (filemtime($this->cacheFile) > (time() - 1 * 60 * 60)) &&
             $info = json_decode(file_get_contents($this->cacheFile), true)
         ) {
             $this->info = $info;
-        }
-        else {
+        } else {
             $this->refresh();
         }
     }
 
-    function refresh()
+    public function refresh()
     {
         $url = 'https://' . $this->team . '.slack.com/api/rtm.start?token=' . $this->token;
         $info = file_get_contents($url);
@@ -38,25 +37,26 @@ class Slack
         fclose($file);
     }
 
-    function getInfo()
+    public function getInfo()
     {
         return $this->info;
     }
 
-    function getImageSrc($width = 132)
+    public function getImageSrc($width = 132)
     {
-        if (! isset($this->info['team']['icon']['image_'.$width])) {
+        if (!isset($this->info['team']['icon']['image_' . $width])) {
             return null;
         }
-        return $this->info['team']['icon']['image_'.$width];
+
+        return $this->info['team']['icon']['image_' . $width];
     }
 
-    function getTeamName()
+    public function getTeamName()
     {
         return $this->info['team']['name'];
     }
 
-    function getUserCount()
+    public function getUserCount()
     {
         $total = 0;
         $active = 0;
@@ -67,14 +67,15 @@ class Slack
                 $active++;
             }
         }
+
         return array('total' => $total, 'active' => $active);
     }
 
-    function invite($email)
+    public function invite($email)
     {
         $url = 'https://' . $this->team . '.slack.com/api/users.admin.invite';
 
-        $data = array ('token' => $this->token, 'email' => $email);
+        $data = array('token' => $this->token, 'email' => $email);
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -84,6 +85,7 @@ class Slack
         $result = curl_exec($ch);
 
         $result = json_decode($result, true);
+
         return $result;
     }
 }
--- www/index.php
+++ www/index.php
@@ -6,7 +6,7 @@ session_name($_ENV['SESSION_NAME']);
 session_start();
 
 $app = new \Slim\Slim([
-	'templates.path' => '../templates'
+    'templates.path' => '../templates'
 ]);
 
 // Environment specific settings
@@ -23,79 +23,86 @@ $app->configureMode('development', function () use ($app) {
 });
 
 // Hook up the Slack instance
-$app->container->singleton('slack', function() {
-	return new Slack($_ENV['SLACK_TEAM'], $_ENV['SLACK_TOKEN']);
+$app->container->singleton('slack', function () {
+    return new Slack($_ENV['SLACK_TEAM'], $_ENV['SLACK_TOKEN']);
 });
 
 
 // The home page here
-$app->get('/', function() use ($app) {
-	// Output the header
-	$app->render('_header.php');
+$app->get('/', function () use ($app) {
+    // Output the header
+    $app->render('_header.php');
 
-	$info = $app->slack->getInfo();
-	$_users = $info['users'];
-	$users = array();
-	foreach ($_users as $user) {
-		if ($user['is_bot'] === true) continue;
-		if ($user['id'] === 'USLACKBOT') continue;
+    $info = $app->slack->getInfo();
+    $_users = $info['users'];
+    $users = array();
+    foreach ($_users as $user) {
+        if ($user['is_bot'] === true) {
+            continue;
+        }
+        if ($user['id'] === 'USLACKBOT') {
+            continue;
+        }
 
-		$users[] = [
-			'id' => $user['id'],
-			'name' => !empty($user['real_name']) ? $user['real_name'] : $user['name'],
-			'active' => $user['presence'] === 'active',
-			'image' => $user['profile']['image_72'],
-		];
-	}
+        $users[] = [
+            'id' => $user['id'],
+            'name' => !empty($user['real_name']) ? $user['real_name'] : $user['name'],
+            'active' => $user['presence'] === 'active',
+            'image' => $user['profile']['image_72'],
+        ];
+    }
 
-	// Sort users by active first
-	uasort($users, function($a, $b) {
-		if ($a['active'] && $b['active']) return 0;
-		if ($a['active']) return -1;
-		if ($b['active']) return 1;
-		return 0;
-	});
-	// Render the page
-	$app->render('slack.php', [
-		'users' => $users,
-		'total' => count($users),
-	]);
+    // Sort users by active first
+    uasort($users, function ($a, $b) {
+        if ($a['active'] && $b['active']) {
+            return 0;
+        }
+        if ($a['active']) {
+            return -1;
+        }
+        if ($b['active']) {
+            return 1;
+        }
 
-	// Output the footer
-	$app->render('_footer.php');
+        return 0;
+    });
+    // Render the page
+    $app->render('slack.php', [
+        'users' => $users,
+        'total' => count($users),
+    ]);
+
+    // Output the footer
+    $app->render('_footer.php');
 })->name('slack');
 
 // POSTing to /slack/invite will send an invite for the requested email address
-$app->post('/slack/invite', function() use ($app) {
-	// Grab the email from the POST
-	$email = $app->request->post('email');
-	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-		$app->flash('invite', 'Please enter a valid email address.');
-		$app->redirect('/');
-	}
+$app->post('/slack/invite', function () use ($app) {
+    // Grab the email from the POST
+    $email = $app->request->post('email');
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $app->flash('invite', 'Please enter a valid email address.');
+        $app->redirect('/');
+    }
 
-	// Send the slack invite 
-	$invited = $app->slack->invite($email);
-	if ($invited['ok'] === true) {
-		$app->flash('invite', 'Your invitation has been sent! Be sure to check your email for further instructions. ');
-	}
-	else {
-		if ($invited['error'] === 'sent_recently') {
-			$app->flash('invite', 'Oops, you already received an invite recently! If you didn\'t get the email, make sure to check your spam folder too.');
-		}
-		elseif ($invited['error'] === 'already_invited') {
-			$app->flash('invite', 'Oops, you were already invited before!');
-		}
-		elseif ($invited['error'] === 'already_in_team') {
-			$app->flash('invite', 'Hey, you are already in the Slack team! Head over to modxcommunity.slack.com if you forgot your details.');
-		}
-		else {
-			$app->flash('invite', 'Oops, something went wrong: ' . json_encode($invited));
-		}
-	}
+    // Send the slack invite
+    $invited = $app->slack->invite($email);
+    if ($invited['ok'] === true) {
+        $app->flash('invite', 'Your invitation has been sent! Be sure to check your email for further instructions. ');
+    } else {
+        if ($invited['error'] === 'sent_recently') {
+            $app->flash('invite', 'Oops, you already received an invite recently! If you didn\'t get the email, make sure to check your spam folder too.');
+        } elseif ($invited['error'] === 'already_invited') {
+            $app->flash('invite', 'Oops, you were already invited before!');
+        } elseif ($invited['error'] === 'already_in_team') {
+            $app->flash('invite', 'Hey, you are already in the Slack team! Head over to modxcommunity.slack.com if you forgot your details.');
+        } else {
+            $app->flash('invite', 'Oops, something went wrong: ' . json_encode($invited));
+        }
+    }
 
-	// Redirect back to the home page; the flash will show a success/error message there.
-	$app->redirect('/');
+    // Redirect back to the home page; the flash will show a success/error message there.
+    $app->redirect('/');
 })->name('slack/invite');
 
 // Run the app
